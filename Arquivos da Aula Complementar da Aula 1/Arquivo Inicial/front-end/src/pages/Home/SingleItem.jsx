1bd4e509b22cde301b4619fc91eb47ea11e13e29
Guto <128807987+gutoportelaa@users.noreply.github.com>
2025-02-12 19:56:46 -0300

    Add files via upload

---
+++ Arquivos da Aula Complementar da Aula 1/Arquivo Inicial/front-end/src/pages/Home/SingleItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
+const SingleItem = ({ path, image, name, type, _id, artist, style }) => {
+  return (
+    <Link to={`${path}/${_id}`} className="single-item">
+      <div className="single-item__div-image">
+        <img
+          src={image}
+          alt={`Imagem de ${name}`}
+          className={`single-item__image ${
+            style === "square" ? "single-item__image--square" : ""
+          }`}
+        />
+
+        <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
+      </div>
+
+      <div className="single-item__texts">
+        <div className="single-item__2lines">
+          <p className="single-item__title">{name}</p>
+        </div>
+        <p className="single-item__type">{type ?? artist}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default SingleItem;

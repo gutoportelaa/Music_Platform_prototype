8906ef775399c4dd9089c2fb8bf47b70e03a7707
Guto <128807987+gutoportelaa@users.noreply.github.com>
2025-02-14 20:18:17 +0000

    scr update

---
+++ Gabarito da Aula 1/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import React from 'react'
+import Main from './components/Main'
+
+export const App = () => {
+return (
+  <>
+   <Header />
+   <Main />
+  </>
+)
+};
+
+export default App;
